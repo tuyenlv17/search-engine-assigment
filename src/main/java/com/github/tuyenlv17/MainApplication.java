94ee767720fc4623f6ec69da8bfe6d0573f18352
Tuyen Luong <tuyenlv17@gmail.com>
2018-12-03 22:31:41 +0700

    init

---
+++ src/main/java/com/github/tuyenlv17/MainApplication.java
@@ -0,0 +1,14 @@
+package com.github.tuyenlv17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tuyenlv17 on 2018-10-09.
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
